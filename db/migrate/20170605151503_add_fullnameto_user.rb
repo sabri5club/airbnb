413ed421d6fc9aa275f6d3cc47afa898f653d84d
sabri <sabri.tanich@gmail.com>
2017-06-05 15:20:38 +0000

    Création du champ fullname dans la table User

---
+++ db/migrate/20170605151503_add_fullnameto_user.rb
@@ -0,0 +1,4 @@
+class AddFullnametoUser < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605140429) do
+ActiveRecord::Schema.define(version: 20170605151503) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
